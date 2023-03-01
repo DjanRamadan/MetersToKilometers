5f1c515109e291925ddd89ded504f9a1b4aad1a0
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 13:07:47 +0200

    Add files via upload

---
+++ MetersToKilometers.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter meters: ");
+double m = double.Parse(Console.ReadLine());
+double mconvertertokm = m / 1000;
+Console.WriteLine($"{m} meters = {mconvertertokm:F2} kilometers");
